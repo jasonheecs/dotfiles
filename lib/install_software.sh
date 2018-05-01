e6ccad7e6c43aa71dcbd9fcff057fe37729c667c
Jason Hee <jasonheecs@gmail.com>
2018-05-02 06:12:20 +0800

    Add rbenv

--- lib/install_software.sh
+++ lib/install_software.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# shellcheck source=./homebrew.sh
+source "./homebrew.sh"
+
 install_nvm() {
+    echo "Installing nvm..."
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
     nvm install node
 }
 
 install_software() {
-    echo 'in'
+    install_homebrew
+
     # shellcheck disable=SC2154
     if ! [[ -x "$(command -v nvm)" ]]; then
         # install_nvm
