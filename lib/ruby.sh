25427d8e7fe997fc198d15e581ff1ab1c4e48de5
Jason Hee <jasonheecs@gmail.com>
2021-06-22 21:24:00 +0800

    New set of dotfiles

--- lib/ruby.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-install_ruby() {
-    LATEST_RUBY_VERSION="$(rbenv install -l | grep -v - | tail -1 | tr -d '[:space:]')"
-    rbenv install "${LATEST_RUBY_VERSION}"
-    rbenv global "${LATEST_RUBY_VERSION}"
-    rbenv rehash
-}
